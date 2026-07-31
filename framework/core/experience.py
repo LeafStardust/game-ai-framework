3d13d824e7334694bf2a70020d1cf52cef280e91
Bryce Yap <98381@siswa.unimas.my>
2026-07-31 15:44:38 +0800

    feat: add experience data structure

---
+++ framework/core/experience.py
@@ -0,0 +1,16 @@
+class Experience:
+    """
+    Stores a single agent interaction.
+    """
+
+    def __init__(
+        self,
+        state,
+        action,
+        reward,
+        next_state
+    ):
+        self.state = state
+        self.action = action
+        self.reward = reward
+        self.next_state = next_state
