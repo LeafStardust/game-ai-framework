d3ad77b2f0d220602cf12713f6d2e157013afb1f
Bryce Yap <98381@siswa.unimas.my>
2026-08-04 15:51:26 +0800

    test: expand game adapter interface tests

--- games/adapter.py
+++ games/adapter.py
@@ -1,4 +1,6 @@
 from framework.core.environment import GameEnvironment
+from framework.core.state import GameState
+from framework.core.action import Action
 
 
 class GameAdapter:
@@ -7,4 +9,18 @@ class GameAdapter:
     """
 
     def create_environment(self) -> GameEnvironment:
+        raise NotImplementedError
+
+
+    def get_state(
+        self,
+        environment: GameEnvironment
+    ) -> GameState:
+        raise NotImplementedError
+
+
+    def get_actions(
+        self,
+        environment: GameEnvironment
+    ) -> list[Action]:
         raise NotImplementedError
--- tests/test_adapter.py
+++ tests/test_adapter.py
@@ -10,3 +10,25 @@ def test_adapter_requires_implementation():
         assert False
     except NotImplementedError:
         assert True
+
+
+def test_adapter_state_requires_implementation():
+
+    adapter = GameAdapter()
+
+    try:
+        adapter.get_state(None)
+        assert False
+    except NotImplementedError:
+        assert True
+
+
+def test_adapter_actions_requires_implementation():
+
+    adapter = GameAdapter()
+
+    try:
+        adapter.get_actions(None)
+        assert False
+    except NotImplementedError:
+        assert True
