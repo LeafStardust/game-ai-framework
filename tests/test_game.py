b540dab5371b049244388dace6ee90d09388fe57
Bryce Yap <98381@siswa.unimas.my>
2026-08-04 16:00:53 +0800

    test: verify game abstraction

--- tests/test_game.py
+++ tests/test_game.py
@@ -4,8 +4,21 @@ from games.dummy.adapter import DummyAdapter
 
 def test_game_creates_environment():
 
+    adapter = DummyAdapter()
+
     game = Game(
-        DummyAdapter()
+        adapter
     )
 
     assert game.environment is not None
+
+
+def test_game_keeps_adapter():
+
+    adapter = DummyAdapter()
+
+    game = Game(
+        adapter
+    )
+
+    assert game.adapter is adapter
