0bcc1b41a8417ff89529707083720fc0a38267e2
Bryce Yap <41868659+LeafStardust@users.noreply.github.com>
2026-08-10 04:09:44 +0800

    Use strongest current hand in discard heuristic

--- games/balatro/heuristics/discard_value.py
+++ games/balatro/heuristics/discard_value.py
@@ -2,7 +2,6 @@ from framework.core.action import Action
 from framework.core.state import GameState
 from framework.evaluation.heuristic import Heuristic
 
-from games.balatro.hand_evaluator import HandEvaluator
 from games.balatro.hand import PokerHand
 from games.balatro.probability import HandProbability
 
@@ -10,10 +9,7 @@ from games.balatro.probability import HandProbability
 class DiscardValueHeuristic(Heuristic):
 
     def __init__(self):
-
-        self.hand_evaluator = HandEvaluator()
         self.probability = HandProbability()
-
 
     def evaluate(
         self,
@@ -24,22 +20,18 @@ class DiscardValueHeuristic(Heuristic):
         if action.name != "DISCARD_CARDS":
             return 0.0
 
-
         discarded_cards = getattr(
             action,
             "cards",
             []
         )
 
-
         if not discarded_cards:
             return 0.0
 
-
-        current_hand = self.hand_evaluator.evaluate(
+        current_hand = self.probability.best_hand(
             state.hand
         )
-
 
         strong_hands = {
             PokerHand.STRAIGHT,
@@ -49,15 +41,12 @@ class DiscardValueHeuristic(Heuristic):
             PokerHand.STRAIGHT_FLUSH
         }
 
-
         if current_hand in strong_hands:
             return -50.0
-
 
         remaining_cards = self.probability.remaining_cards(
             state.deck
         )
-
 
         improvement_chance = self.probability.draw_probability(
             len(discarded_cards),
@@ -65,9 +54,7 @@ class DiscardValueHeuristic(Heuristic):
             1
         )
 
-
         score = improvement_chance * 20
-
 
         if current_hand == PokerHand.HIGH_CARD:
             score += 10
@@ -75,13 +62,11 @@ class DiscardValueHeuristic(Heuristic):
         elif current_hand == PokerHand.PAIR:
             score += 5
 
-
         if getattr(
             state,
             "discards_remaining",
             0
         ) <= 1:
             score -= 10
-
 
         return score
