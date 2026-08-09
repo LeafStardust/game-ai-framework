932f4ba928f91cbea78183b3a26489b7e06c67ce
Bryce Yap <41868659+LeafStardust@users.noreply.github.com>
2026-08-10 04:59:10 +0800

    Add stake selection to Balatro adapter

--- games/balatro/adapter.py
+++ games/balatro/adapter.py
@@ -4,20 +4,24 @@ from framework.core.action import Action
 
 from games.adapter import GameAdapter
 from games.balatro.decks import BASE_DECK, BalatroDeck
-from games.balatro.environment import BalatroEnvironment
+from games.balatro.stake_environment import BalatroStakeEnvironment
+from games.balatro.stakes import WHITE_STAKE, BalatroStake
 
 
 class BalatroAdapter(GameAdapter):
 
     def __init__(
         self,
-        deck: BalatroDeck = BASE_DECK
+        deck: BalatroDeck = BASE_DECK,
+        stake: BalatroStake = WHITE_STAKE
     ):
         self.deck = deck
+        self.stake = stake
 
     def create_environment(self) -> GameEnvironment:
-        return BalatroEnvironment(
-            self.deck
+        return BalatroStakeEnvironment(
+            self.deck,
+            self.stake
         )
 
     def get_state(
