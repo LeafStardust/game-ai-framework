35e1829ff23ac5a3d78971607717d9d7167ff974
Bryce Yap <98381@siswa.unimas.my>
2026-08-10 02:18:13 +0800

    test: cover planet consumable usage

--- tests/test_balatro_planets.py
+++ tests/test_balatro_planets.py
@@ -1,4 +1,7 @@
 from games.balatro.planets import create_planet, random_planet
+from games.balatro.consumable import ConsumableContext
+from games.balatro.state import BalatroState
+from games.balatro.planets import create_planet, random_planet
 
 
 def test_create_planet_returns_independent_instance():
@@ -29,3 +32,35 @@ def test_random_planet_uses_rng():
     )
 
     assert planet.name == "Pluto"
+
+
+def test_planet_can_use_without_cards():
+
+    state = BalatroState()
+
+    planet = create_planet(
+        "PLUTO"
+    )
+
+    context = ConsumableContext(
+        state=state
+    )
+
+    assert planet.can_use(context)
+
+
+def test_planet_use_levels_up_hand():
+
+    state = BalatroState()
+
+    planet = create_planet(
+        "PLUTO"
+    )
+
+    context = ConsumableContext(
+        state=state
+    )
+
+    planet.use(context)
+
+    assert state.hand_levels["HIGH_CARD"] == 2
