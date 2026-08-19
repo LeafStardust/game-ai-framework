1635a0a50cc4bff53b0c3407a6497b399e29aed3
Bryce Yap <41868659+LeafStardust@users.noreply.github.com>
2026-08-19 21:25:24 +0800

    feat: install ten-attempt calibration policies

--- games/balatro/__init__.py
+++ games/balatro/__init__.py
@@ -4,6 +4,8 @@ from games.balatro.aces_scholar_policy import install_aces_scholar_policy
 from games.balatro.blue_joker_strategy_rules import apply_blue_joker_strategy_rules
 from games.balatro.card_sharp_strategy_rules import install_card_sharp_strategy_rules
 from games.balatro.constellation_strategy_rules import apply_constellation_strategy_rules
+from games.balatro.d1_log_resilience_policy import install_d1_log_resilience_policy
+from games.balatro.log_batch_calibration_policy import install_log_batch_calibration_policy
 from games.balatro.mouth_hand_policy import install_mouth_first_hand_policy
 from games.balatro.pareidolia_face_policy import install_pareidolia_face_policy
 from games.balatro.strategy_multi_engine import install_multi_engine_strategy_policy
@@ -16,3 +18,5 @@ install_mouth_first_hand_policy()
 install_aces_scholar_policy()
 install_pareidolia_face_policy()
 install_card_sharp_strategy_rules()
+install_d1_log_resilience_policy()
+install_log_batch_calibration_policy()
