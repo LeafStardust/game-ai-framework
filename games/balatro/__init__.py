11e9e51edd42a2b2f4bf6546076d27e37ab7dace
Bryce Yap <41868659+LeafStardust@users.noreply.github.com>
2026-08-20 00:53:42 +0800

    feat: install safe pace survival optimizations

--- games/balatro/__init__.py
+++ games/balatro/__init__.py
@@ -8,6 +8,7 @@ from games.balatro.d1_log_resilience_policy import install_d1_log_resilience_pol
 from games.balatro.log_batch_calibration_policy import install_log_batch_calibration_policy
 from games.balatro.mouth_hand_policy import install_mouth_first_hand_policy
 from games.balatro.pareidolia_face_policy import install_pareidolia_face_policy
+from games.balatro.safe_pace_optimization_policy import install_safe_pace_optimization_policy
 from games.balatro.shop_regression_policy import install_shop_regression_policy
 from games.balatro.strategy_commitment_hysteresis import install_strategy_commitment_hysteresis
 from games.balatro.strategy_multi_engine import install_multi_engine_strategy_policy
@@ -26,3 +27,4 @@ install_log_batch_calibration_policy()
 install_shop_regression_policy()
 install_three_run_review_policy()
 install_strategy_commitment_hysteresis()
+install_safe_pace_optimization_policy()
