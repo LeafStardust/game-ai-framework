ebe74b062c509290fe02d096596da48727a60825
Bryce Yap <41868659+LeafStardust@users.noreply.github.com>
2026-08-19 16:39:06 +0800

    feat: install multi-engine strategy orchestration

--- games/balatro/__init__.py
+++ games/balatro/__init__.py
@@ -1,6 +1,8 @@
 """Balatro package policy registration."""
 
 from games.balatro.blue_joker_strategy_rules import apply_blue_joker_strategy_rules
+from games.balatro.strategy_multi_engine import install_multi_engine_strategy_policy
 
 
 apply_blue_joker_strategy_rules()
+install_multi_engine_strategy_policy()
