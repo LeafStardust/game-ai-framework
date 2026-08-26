f03ed97f37d183c264605d6779788bcdafad7a81
Bryce Yap <41868659+LeafStardust@users.noreply.github.com>
2026-08-26 15:01:26 +0800

    fix(balatro): install High Priestess pack expectation

--- games/balatro/__init__.py
+++ games/balatro/__init__.py
@@ -31,6 +31,7 @@ from games.balatro.forming_strategy_retention_policy import install_forming_stra
 from games.balatro.full_roster_pack_guard import install_full_roster_pack_guard
 from games.balatro.full_roster_shop_guard import install_full_roster_shop_guard
 from games.balatro.held_round_end_resource_policy import install_held_round_end_resource_policy
+from games.balatro.high_priestess_pack_policy import install_high_priestess_pack_policy
 from games.balatro.hook_planner_integration_policy import install_hook_planner_integration_policy
 from games.balatro.late_shop_resource_guard import install_late_shop_resource_guard
 from games.balatro.strategy_execution_guard_policy import install_strategy_execution_guard_policy
@@ -94,6 +95,7 @@ install_late_shop_resource_guard()
 install_castle_discard_policy()
 install_deck_growth_pack_policy()
 install_pack_sunk_cost_policy()
+install_high_priestess_pack_policy()
 install_tactical_scaler_build_health_policy()
 install_bond_scaler_build_health_policy()
 install_build_health_policy()
