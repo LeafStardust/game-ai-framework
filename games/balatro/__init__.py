40797b12cb421a0bfa933b5aca261c02a581d090
Bryce Yap <41868659+LeafStardust@users.noreply.github.com>
2026-08-26 19:10:35 +0800

    fix(balatro): install literal deck-growth card value

--- games/balatro/__init__.py
+++ games/balatro/__init__.py
@@ -28,6 +28,7 @@ from games.balatro.d1_debuff_recovery_policy import install_d1_debuff_recovery_p
 from games.balatro.d1_log_resilience_policy import install_d1_log_resilience_policy
 from games.balatro.d1_outer_evaluation_cache_policy import install_d1_outer_evaluation_cache_policy
 from games.balatro.deck_growth_pack_policy import install_deck_growth_pack_policy
+from games.balatro.deck_growth_playing_card_value_policy import install_deck_growth_playing_card_value_policy
 from games.balatro.early_capacity_policy import install_early_capacity_policy
 from games.balatro.early_spend_sanity_policy import install_early_spend_sanity_policy
 from games.balatro.ectoplasm_live_state_policy import install_ectoplasm_live_state_policy
@@ -127,6 +128,7 @@ install_early_spend_sanity_policy()
 install_late_shop_resource_guard()
 install_castle_discard_policy()
 install_deck_growth_pack_policy()
+install_deck_growth_playing_card_value_policy()
 install_pack_sunk_cost_policy()
 install_high_priestess_pack_policy()
 install_immolate_pack_expectation_policy()
