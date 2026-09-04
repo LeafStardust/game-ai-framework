a62b53a959720baba6070b914288006867aa575f
Bryce <41868659+LeafStardust@users.noreply.github.com>
2026-09-04 13:35:26 +0800

    test(balatro): include Arm in start-inert boss set

--- tests/balatro/test_balatro_env_r2_start_inert_boss.py
+++ tests/balatro/test_balatro_env_r2_start_inert_boss.py
@@ -12,7 +12,14 @@ from games.balatro.env.transition import HeadlessRunState, HeadlessTransitionErr
 from games.balatro.state import BalatroState
 
 
-START_INERT_BOSSES = ("The Psychic", "The Flint", "The Tooth", "The Hook", "The Ox")
+START_INERT_BOSSES = (
+    "The Psychic",
+    "The Flint",
+    "The Tooth",
+    "The Hook",
+    "The Ox",
+    "The Arm",
+)
 
 
 def _run(boss_name: str) -> HeadlessRunState:
